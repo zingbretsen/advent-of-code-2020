caf569b56b2191288b0433f17e470ce5e68995aa
Zach Ingbretsen <zach@zingbretsen.com>
2020-12-05 09:26:29 -0500

    Simplifies day 5

--- day05.py
+++ day05.py
@@ -2,10 +2,9 @@ from aocd import get_data, submit
 
 
 def parse_line(line):
-    row, col = line[:-3], line[-3:]
-    row = row.replace("F", "0").replace("B", "1")
-    col = col.replace("L", "0").replace("R", "1")
-    return int(row, 2), int(col, 2)
+    line = line.replace("F", "0").replace("B", "1")
+    line = line.replace("L", "0").replace("R", "1")
+    return int(line, 2)
 
 
 def find_seat(seats):
@@ -17,19 +16,12 @@ def find_seat(seats):
         prev_seat = seat
 
 
-def get_seat_ids(seats):
-    return [row * 8 + col for row, col in seats]
-
-
 if __name__ == "__main__":
     d = get_data()
     inputs = [parse_line(line) for line in d.split("\n")]
 
-    answer_a = max(row * 8 + col for row, col in inputs)
+    answer_a = max(inputs)
     submit(answer_a, part="a", day=5, year=2020)
 
-    seats = get_seat_ids(inputs)
-    answer_b = find_seat(seats)
-
-    print(answer_b)
+    answer_b = find_seat(inputs)
     submit(answer_b, part="b", day=5, year=2020)
