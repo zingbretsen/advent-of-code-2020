5bfa8ad7cfd592b7e0c727d1b0222bd43a2803fd
Zach Ingbretsen <zach@zingbretsen.com>
2020-12-10 19:45:52 -0500

    Adds working day 10

---
+++ day10.py
@@ -0,0 +1,32 @@
+import math
+from aocd import get_data, submit
+
+
+def find_diffs(inputs):
+    inputs = sorted(inputs)
+    diffs = [inputs[0]]
+    for x, y in zip(inputs[:-1], inputs[1:]):
+        diffs.append(y - x)
+    diffs.append(3)
+    return diffs
+
+
+def find_spans(diffs):
+    diffs = [str(n) for n in diffs]
+    s = [len(ones) for ones in "".join(diffs).split("3")]
+    mappings = {2: 2, 3: 4, 4: 7}
+    s = [mappings[x] for x in s if x > 1]
+    return s
+
+
+if __name__ == "__main__":
+    d = get_data()
+    inputs = [int(line) for line in d.split("\n") if line]
+    diffs = find_diffs(inputs)
+
+    answer_a = diffs.count(1) * diffs.count(3)
+    submit(answer_a, part="a", day=10, year=2020)
+
+    s = find_spans(diffs)
+    answer_b = math.prod(s)
+    submit(answer_b, part="b", day=10, year=2020)
