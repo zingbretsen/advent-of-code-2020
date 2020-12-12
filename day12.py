f36dce437c4de613f6045d0781d4fe9003a70662
Zach Ingbretsen <zach@zingbretsen.com>
2020-12-12 11:11:28 -0500

    Simplifies with inheritance and complex rotations

--- day12.py
+++ day12.py
@@ -1,11 +1,14 @@
 from aocd import get_data, submit
 
+MOVES = {"N": 1j, "S": -1j, "E": 1, "W": -1}
+ROTATIONS = {"L": 0 + 1j, "R": 0 - 1j}
+
 
 class Ferry:
     def __init__(self, instructions):
         self.instructions = instructions
-        self.position = complex(0, 0)
-        self.heading = complex(1, 0)
+        self.position = 0 + 0j
+        self.heading = 1 + 0j
         self.letter = None
         self.amount = None
 
@@ -14,24 +17,11 @@ class Ferry:
         self.amount = int(instruction[1:])
 
     def rotate(self):
-        if self.letter == "L":
-            self.letter = "R"
-            self.amount = (360 - self.amount) % 360
-        while self.amount > 0:
-            self.heading *= complex(0, -1)
-            self.amount -= 90
+        for _ in range(int(self.amount / 90)):
+            self.heading *= ROTATIONS[self.letter]
 
     def translate(self):
-        if self.letter in "SW":
-            sign = -1
-        else:
-            sign = 1
-        if self.letter in "NS":
-            heading = sign * complex(0, 1)
-        else:
-            heading = sign * complex(1, 0)
-
-        self.position += self.amount * heading
+        self.position += self.amount * MOVES[self.letter]
 
     def forward(self):
         self.position += self.amount * self.heading
@@ -50,61 +40,26 @@ class Ferry:
         return int(abs(self.position.imag) + abs(self.position.real))
 
 
-class Waypoint:
+class Waypoint(Ferry):
     def __init__(self, instructions):
-        self.instructions = instructions
-        self.position = complex(10, 1)
-        self.rotation = complex(0, 0)
-        self.letter = None
-        self.amount = None
+        super().__init__(instructions)
+        self.position = 10 + 1j
         self.ferry = Ferry([])
 
-    def parse_instruction(self, instruction):
-        self.letter = instruction[0]
-        self.amount = int(instruction[1:])
-
     def rotate(self):
-        if self.letter == "L":
-            self.letter = "R"
-            self.amount = (360 - self.amount) % 360
-        while self.amount > 0:
-            self.position *= complex(0, -1)
-            self.amount -= 90
-
-    def translate(self):
-        if self.letter in "SW":
-            sign = -1
-        else:
-            sign = 1
-        if self.letter in "NS":
-            rotation = sign * complex(0, 1)
-        else:
-            rotation = sign * complex(1, 0)
-
-        self.position += self.amount * rotation
+        for _ in range(int(self.amount / 90)):
+            self.position *= ROTATIONS[self.letter]
 
     def forward(self):
         self.ferry.amount = self.amount
         self.ferry.heading = self.position
         self.ferry.forward()
 
-    def follow_instructions(self):
-        for instruction in self.instructions:
-            self.parse_instruction(instruction)
-            if self.letter in "LR":
-                self.rotate()
-            elif self.letter in "NSEW":
-                self.translate()
-            else:
-                self.forward()
-
-    def calc_distance_from_start(self):
-        return self.ferry.calc_distance_from_start()
-
 
 if __name__ == "__main__":
     d = get_data()
     inputs = [line for line in d.split("\n") if line]
+
     f = Ferry(inputs)
     f.follow_instructions()
     answer_a = f.calc_distance_from_start()
@@ -112,5 +67,5 @@ if __name__ == "__main__":
 
     w = Waypoint(inputs)
     w.follow_instructions()
-    answer_b = w.calc_distance_from_start()
+    answer_b = w.ferry.calc_distance_from_start()
     submit(answer_b, part="b", day=12, year=2020)
