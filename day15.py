511ba0498578117e290c9b6e9446fb230e435868
Zach Ingbretsen <zach@zingbretsen.com>
2020-12-15 08:37:50 -0500

    Cleanup day 15

--- day15.py
+++ day15.py
@@ -1,5 +1,3 @@
-from itertools import product
-from collections import deque
 from aocd import get_data, submit
 
 
@@ -12,8 +10,8 @@ def find_nth_num(nums, n):
         num_dict[num] = i
 
     for i in range(len(nums), n):
-        num_dict[new_num], idx = i, num_dict.get(new_num, i)
-        # = i
+        idx = num_dict.get(new_num, i)
+        num_dict[new_num] = i
         prev_num, new_num = new_num, i - idx
 
     return prev_num
@@ -23,12 +21,8 @@ if __name__ == "__main__":
     d = get_data()
     nums = [int(num) for num in d.split(",") if num]
 
-    # answer_a: 289
     answer_a = find_nth_num(nums, 2020)
-    assert answer_a == 289, "Wrong A"
     submit(answer_a, part="a", day=15, year=2020)
 
-    # answer_b: 1505722
     answer_b = find_nth_num(nums, 30000000)
-    assert answer_b == 1505722, "Wrong B"
     submit(answer_b, part="b", day=15, year=2020)
