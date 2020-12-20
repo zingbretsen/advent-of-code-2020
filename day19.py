815bc466a2b98422d3dd522bb35143809f5f0fd6
Zach Ingbretsen <zach@zingbretsen.com>
2020-12-19 19:37:28 -0500

    Day 19 cleaned up

--- day19.py
+++ day19.py
@@ -1,11 +1,9 @@
 from aocd import get_data, submit
+from string import ascii_lowercase
 from itertools import product
 
 
-if __name__ == "__main__":
-    rules, messages = get_data().split("\n\n")
-    messages = messages.split("\n")
-
+def derive_rules(rules):
     rule_dict = dict()
     for rule in rules.split("\n"):
         key, value = rule.split(": ")
@@ -15,25 +13,26 @@ if __name__ == "__main__":
             values = value.split(" | ")
             value = [list(map(int, v.split())) for v in values]
         rule_dict[key] = value
+    return rule_dict
 
-    longest_message = max(len(m) for m in messages)
 
-    def x(n=1, exceptions=(8, 11), depth=1):
-        value = rule_dict[n]
-        if type(value) is str:
-            return [value]
-        else:
-            out_list = []
-            for option in value:
-                out = []
-                for v in option:
-                    o = x(v, depth=depth + 1)
-                    out.append(o)
-                out = ["".join(p) for p in product(*out)]
-                out_list.extend(out)
-            return out_list
+def find_all_matches(rule_dict, n=0):
+    value = rule_dict[n]
+    if type(value) is str:
+        return [value]
+    else:
+        out_list = []
+        for option in value:
+            out = []
+            for v in option:
+                o = find_all_matches(rule_dict, v)
+                out.append(o)
+            out = ["".join(p) for p in product(*out)]
+            out_list.extend(out)
+        return out_list
 
-    all_matches = set(x(0))
+
+def count_matches(messages, all_matches):
     c = 0
     invalid_messages = set()
     for message in messages:
@@ -41,18 +40,20 @@ if __name__ == "__main__":
             c += 1
         else:
             invalid_messages.add(message)
+    return c, invalid_messages
 
-    submit(c, part="a", day=19, year=2020)
 
+def count_expanded_matches(invalid_messages):
     # Part B must start with one or more 42 and end with one or more 31
     # The number of 31s must be at most the number of 42s minus one
-    forty_two = x(42)
-    thirty_one = x(31)
+    forty_two = find_all_matches(rule_dict, 42)
+    thirty_one = find_all_matches(rule_dict, 31)
 
     # Fortunately, all of the patterns are 8 chars long
     l1 = len(forty_two[0])
     l2 = len(thirty_one[0])
 
+    c = 0
     for vm in invalid_messages:
         a = b = 0
         # Find all strings that match any of the patterns from rule 31
@@ -72,5 +73,20 @@ if __name__ == "__main__":
         # If the string is entirely composed of 42s and 31s, it is a match
         if a > b > 0 and vm == "":
             c += 1
+    return c
 
+
+if __name__ == "__main__":
+    rules, messages = get_data().split("\n\n")
+    messages = messages.split("\n")
+
+    rule_dict = derive_rules(rules)
+    all_matches = set(find_all_matches(rule_dict, n=0))
+
+    c, invalid_messages = count_matches(messages, all_matches)
+    print(c)
+    submit(c, part="a", day=19, year=2020)
+
+    c += count_expanded_matches(invalid_messages)
+    print(c)
     submit(c, part="b", day=19, year=2020)
