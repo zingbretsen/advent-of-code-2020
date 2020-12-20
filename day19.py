85a610de80251d706c542d287d1dfadfda63f1bc
Zach Ingbretsen <zach@zingbretsen.com>
2020-12-19 19:14:27 -0500

    Working day 19 parts A and B

--- day19.py
+++ day19.py
@@ -1,57 +1,11 @@
-import re
-from string import ascii_lowercase
-from operator import add, mul
-
 from aocd import get_data, submit
-
-
-def find_all_matches(rules, n="0"):
-    rule = rules[n]
-    all_rules = []
-    while rule not in ascii_lowercase:
-        for n in rule:
-            find_all_matches(rules, n)
-    return all_rules
-
-
 from itertools import product
 
 
-def x(rules, n=1):
-    value = rules[n]
-    print("XXX", n, value)
-    if type(value) is str:
-        return [value]
-    else:
-        out_list = []
-        for option in value:
-            out = []
-            for v in option:
-                o = x(rules, v)
-                out.append(o)
-            print("out: ", out)
-            breakpoint()
-            out_list.append(list(product(*out)))
-        print("out_list: ", out_list)
-        return out_list
-        # return list(product(*out_list))
-
-
-print(x(rule_dict, 2))
-
 if __name__ == "__main__":
     rules, messages = get_data().split("\n\n")
-    rules = '''0: 4 1 5
-1: 2 3 | 3 2
-2: 4 4 | 5 5
-3: 4 5 | 5 4
-4: "a"
-5: "b"'''
+    messages = messages.split("\n")
 
-    rules = '''0: 1 2
-1: "a"
-2: 1 3 | 3 1
-3: "b"'''
     rule_dict = dict()
     for rule in rules.split("\n"):
         key, value = rule.split(": ")
@@ -62,11 +16,61 @@ if __name__ == "__main__":
             value = [list(map(int, v.split())) for v in values]
         rule_dict[key] = value
 
-    rules = {rule.split(": ")[0]: rule.split(": ")[1].split()}
-    all_matches = find_all_matches(rules)
+    longest_message = max(len(m) for m in messages)
 
-    answer_a = sum(parse_equation(e) for e in lines)
-    submit(answer_a, part="a", day=18, year=2020)
+    def x(n=1, exceptions=(8, 11), depth=1):
+        value = rule_dict[n]
+        if type(value) is str:
+            return [value]
+        else:
+            out_list = []
+            for option in value:
+                out = []
+                for v in option:
+                    o = x(v, depth=depth + 1)
+                    out.append(o)
+                out = ["".join(p) for p in product(*out)]
+                out_list.extend(out)
+            return out_list
 
-    answer_b = sum(parse_equation_advanced(e) for e in lines)
-    submit(answer_b, part="b", day=18, year=2020)
+    all_matches = set(x(0))
+    c = 0
+    invalid_messages = set()
+    for message in messages:
+        if message in all_matches:
+            c += 1
+        else:
+            invalid_messages.add(message)
+
+    submit(c, part="a", day=19, year=2020)
+
+    # Part B must start with one or more 42 and end with one or more 31
+    # The number of 31s must be at most the number of 42s minus one
+    forty_two = x(42)
+    thirty_one = x(31)
+
+    # Fortunately, all of the patterns are 8 chars long
+    l1 = len(forty_two[0])
+    l2 = len(thirty_one[0])
+
+    for vm in invalid_messages:
+        a = b = 0
+        # Find all strings that match any of the patterns from rule 31
+        # at the end of the string
+        while len(vm) > 0 and vm[-l2:] in thirty_one:
+            b += 1
+            vm = vm[:-l2]
+        # There should be at least 1
+        if b > 0:
+            # Find all strings that match any of the patterns from rule 42
+            # at the beginning of the string
+            while vm != "" and vm[:l1] in forty_two:
+                a += 1
+                vm = vm[l1:]
+
+        # There should be at least one more 42 than 31
+        # If the string is entirely composed of 42s and 31s, it is a match
+        if a > b > 0 and vm == "":
+            c += 1
+
+    submit(c, part="b", day=19, year=2020)
