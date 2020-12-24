cab6587c2f0ed8413b81b44038e8e26da690c08e
Zach Ingbretsen <zach@zingbretsen.com>
2020-12-23 20:32:57 -0500

    Working day 23 part B

--- day23.py
+++ day23.py
@@ -11,34 +11,79 @@ def get_dest_label(label, buff, mn, mx):
     return label
 
 
-def get_dest_idx(d, label):
-    return d.index(label) + 1
+def insert_three(d, dest_label, buff):
+    next_label = d[dest_label]
+    d[dest_label] = buff[0]
+    d[buff[-1]] = next_label
 
 
-def iterate(d, mn=1, mx=9, **kwargs):
-    dest_label = d[0] - 1
+def iterate(d, current, mn=1, mx=9, p=False):
+    dest_label = current - 1
     if dest_label < mn:
         dest_label = mx
-    d.rotate(-1)
-    buff = []
-    for _ in range(3):
-        buff.append(d.popleft())
+    buff, next_label = cut_next_three(d, current)
     dest_label = get_dest_label(dest_label, buff, mn, mx)
-    dest_idx = get_dest_idx(d, dest_label, **kwargs)
-    for b in reversed(buff):
-        d.insert(dest_idx, b)
+    insert_three(d, dest_label, buff)
+    return d[current]
+
+
+def cut_next_three(d, starting_label):
+    excised_label = d[starting_label]
+    middle_label = d[excised_label]
+    last_excised_label = d[middle_label]
+    next_label = d[last_excised_label]
+    d[starting_label] = next_label
+    return (excised_label, middle_label, last_excised_label), next_label
+
+
+def create_initial(d):
+    last_label = d[-1]
+    first_label = d[0]
+    d = {label: next_label for label, next_label in zip(d, d[1:] + [d[0]])}
+    d[last_label] = first_label
+    return d
+
+
+def find_answer(d, starting=1):
+    answer = ""
+    next_label = d[starting]
+    answer += str(next_label)
+    while next_label != starting:
+        next_label = d[next_label]
+        answer += str(next_label)
+    return answer[:-1]
+
+
+def print_n(d, n, starting):
+    print()
+    print(starting)
+    for _ in range(n):
+        starting = d[starting]
+        print(starting)
+
+
+def find_answer_b(d, starting=1):
+    next_1 = d[starting]
+    next_2 = d[next_1]
+    print(next_1, next_2, next_1 * next_2)
+    return next_1 * next_2
 
 
 if __name__ == "__main__":
+    data = list(map(int, get_data(day=23)))
 
-    d = get_data(day=23)
-    d = deque(map(int, d))
+    current = data[0]
+    d1 = create_initial(data)
+    d2 = create_initial(data + list(range(10, 1_000_001)))
 
     for _ in range(100):
-        iterate(d)
-    while d[0] != 1:
-        d.rotate(1)
-    answer_a = int("".join(map(str, list(d)[1:])))
-    print(answer_a)
-
+        current = iterate(d1, current)
+    answer_a = find_answer(d1)
     submit(answer_a, part="a", day=23, year=2020)
+
+    current = data[0]
+    for i in range(10_000_000):
+        current = iterate(d2, current, mn=1, mx=1_000_000)
+
+    answer_b = find_answer_b(d2)
+    submit(answer_b, part="b", day=23, year=2020)
